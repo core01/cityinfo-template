26711f0c5ccc7e2725af1b13768752a6abaa43c8
Roman Sadoyan <ra@sadoyan.ru>
2019-03-17 23:26:12 +0600

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,50 @@
+const tailwind = require('tailwindcss')('./tailwind.js')
+const purgeCSS = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './src/pug/**/*.pug'
+  ],
+  whitelist: ['html', 'body'],
+  whitelistPatternsChildren: [],
+  extractors: [
+    {
+      extractor: class TailwindExtractor {
+        static extract (content) {
+          return content.match(/[A-Za-z0-9-_:/]+/g) || []
+        }
+      },
+      // Specify the file extensions to include when scanning for
+      // class names.
+      extensions: ['html', 'js', 'pug']
+    }
+  ]
+})
+
+const simpleVars = require('postcss-simple-vars')
+const cssImport = require('postcss-import')
+const autoprefixer = require('autoprefixer')({
+  browsers: ['last 2 versions', 'iOS >= 8']
+})
+const cssnano = require('cssnano')({
+  preset: [
+    'default',
+    {
+      discardComments: {
+        removeAll: true
+      }
+    }
+  ]
+})
+
+const plugins = [tailwind, simpleVars, cssImport]
+
+module.exports = ({ env }) => {
+  if (env !== 'development') {
+    plugins.push(purgeCSS)
+  }
+
+  plugins.push(autoprefixer, cssnano)
+
+  return {
+    plugins: plugins
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const fs = require('fs')
+
+const generateHtmlPlugins = templateDir => {
+  const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir))
+  return templateFiles.filter(item => {
+    const parts = item.split('.')
+
+    return !!parts[1]
+  }).map(item => {
+    const parts = item.split('.')
+    const name = parts[0]
+    const extension = parts[1]
+
+    return new HtmlWebpackPlugin({
+      filename: `${name}.html`,
+      template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
+      inject: false
+    })
+  })
+}
+
+const htmlPlugins = generateHtmlPlugins('./src/pug')
+
+module.exports = {
+  entry: [
+    './src/js/main.js',
+    './src/css/main.css'
+  ],
+  output: {
+    filename: './js/main.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: path.resolve(__dirname, 'css/')
+            }
+          },
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 1 }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              config: {
+                path: path.resolve(__dirname, 'postcss.config.js')
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.pug$/,
+        use: ['html-loader', 'pug-html-loader?pretty&exports=false']
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './css/[name].css',
+      chunkFilename: '[id].css'
+    })
+  ].concat(htmlPlugins)
+}
